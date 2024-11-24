04983906b6cff30aafe6e6b70a669fcfc2aa067d
Hannele Ruiz <justlemoncl@gmail.com>
2024-11-24 14:49:14 -0300

    Added missing static keyword

--- source/scripting_v3/GTA/Console.cs
+++ source/scripting_v3/GTA/Console.cs
@@ -1,29 +1,29 @@
 ﻿namespace GTA
 {
-    public class Console
+    public static class Console
     {
         /// <summary>
         /// Gets a value indicating whether the console is open.
         /// </summary>
-        public bool IsOpen => ScriptHookVDotNet.GetConsole().IsOpen;
+        public static bool IsOpen => ScriptHookVDotNet.GetConsole().IsOpen;
 
         /// <summary>
         /// Writes the specified message to the console with the level set to INFO.
         /// </summary>
         /// <param name="message">The message to print.</param>
         /// <param name="args">The arguments used to format the message, if any.</param>
-        public void WriteInfo(string message, params object[] args) => ScriptHookVDotNet.GetConsole().PrintInfo(message, args);
+        public static void WriteInfo(string message, params object[] args) => ScriptHookVDotNet.GetConsole().PrintInfo(message, args);
         /// <summary>
         /// Writes the specified message to the console with the level set to WARNING.
         /// </summary>
         /// <param name="message">The message to print.</param>
         /// <param name="args">The arguments used to format the message, if any.</param>
-        public void WriteWarning(string message, params object[] args) => ScriptHookVDotNet.GetConsole().PrintWarning(message, args);
+        public static void WriteWarning(string message, params object[] args) => ScriptHookVDotNet.GetConsole().PrintWarning(message, args);
         /// <summary>
         /// Writes the specified message to the console with the level set to ERROR.
         /// </summary>
         /// <param name="message">The message to print.</param>
         /// <param name="args">The arguments used to format the message, if any.</param>
-        public void WriteError(string message, params object[] args) => ScriptHookVDotNet.GetConsole().PrintError(message, args);
+        public static void WriteError(string message, params object[] args) => ScriptHookVDotNet.GetConsole().PrintError(message, args);
     }
 }
