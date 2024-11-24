768f471517d87f913693c46e9bc1effb615eb55c
Hannele Ruiz <justlemoncl@gmail.com>
2024-11-24 14:29:38 -0300

    Added property to get whether the console is open

--- source/scripting_v3/GTA/Console.cs
+++ source/scripting_v3/GTA/Console.cs
@@ -2,5 +2,9 @@
 {
     public class Console
     {
+        /// <summary>
+        /// Gets a value indicating whether the console is open.
+        /// </summary>
+        public bool IsOpen => ScriptHookVDotNet.GetConsole().IsOpen;
     }
 }
